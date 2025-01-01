39b62e0f580fe3bf4a8275c40cdbb3651a81d248
ahmerdnaseeru <ahmerdnaseeru@gmail.com>
2025-01-01 21:40:23 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 const version = require("@whiskeysockets/baileys/package.json").version
 //Input number for pair code
-global.pairingNumber = "2349019529423"
+global.pairingNumber = "2349120241895"
 
-global.botNumber = ["2349019529423"]
+global.botNumber = ["2349120241895"]
 global.domain = "https://.nobodysey.me"
 global.apikey = "ptla_DfGlmoY7jCzmkNUgyK925FPVE08Kr4mxwLMep"
 global.capikey = "ptlc_GTm42E3qIwbLW3K9w3htljUvhGvnG71hoPTqE"
@@ -11,18 +11,18 @@ global.loc = "1"
 global.anticall = false
 global.autoReadChat = false
 global.alwaysonline = true
-global.autoswview = false
+global.autoswview = true
 global.public = true
 global.autoreact = false
 global.antidelete = false
-global.autotyping = false
+global.autotyping = true
 global.autoBio = true
 global.antiSpam = true
 global.sign = "☃️❄️"
 // ☞ ➻ ➽ ☛ 〆 
 global.bank = "Opay"
 global.bankname = "BLUE"
-global.accnumber = "7041039367"
+global.accnumber = "8064142171"
 global.language = "en"
 global.sessionName = "session"
 global.pairingCode = true 
